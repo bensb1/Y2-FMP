f9a073d3e75cfd0e0f40a90fe78b91552403fecc
bensb1 <56164275+bensb1@users.noreply.github.com>
2022-02-27 17:07:11 +0000

    first tutiraiol done

---
+++ Assets/Spaceship.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    Rigidbody2D rb;
+     public float speed;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal")*speed,0));
+        rb.AddForce(new Vector2(0,Input.GetAxis("Vertical") * speed));
+    }
+}
