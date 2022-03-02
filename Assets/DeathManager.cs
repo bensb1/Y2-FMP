a69f73ab432e6bf0cb1e8e37b6ee4cbd4851fe1f
bensb1 <162532@chrome.escg.ac.uk>
2022-03-02 13:52:49 +0000

    adding in deathmanager

---
+++ Assets/DeathManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathManager : MonoBehaviour
+{
+    GameObject player;
+    bool gameOver = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player==null&&!gameOver)
+        {
+            GameOver();
+            StartCoroutine(LoadGameOver());
+        }
+    }
+    void GameOver()
+    {
+        gameOver = true;
+    }
+    IEnumerator LoadGameOver()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
