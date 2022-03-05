d2277fc450e2329c975c3dbfcf4050b99a4c4f8d
bensb1 <56164275+bensb1@users.noreply.github.com>
2022-03-05 09:15:59 +0000

    Revert "trying to add x axis movement"
    This reverts commit 156911b0d1e5f23215e958600388bf05e8fd71c2.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float moveSpeed = 15f ;
-    public bool changeDirection = false;
     public GameObject bullet,explosion,Extralife;
     Rigidbody2D rb;
     public Color bulletcolor;
@@ -34,22 +32,10 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         rb.velocity = new Vector2(xSpeed,ySpeed*-1);
-        moveEnemy();
     }
     private void OnCollisionEnter2D(Collision2D col)
     {
-        if(col.gameObject.name == "RightWall")
-        {
-            Debug.Log("Hit the Right Wall");
-            changeDirection = true;
-        }
-        if (col.gameObject.name == "LeftWall")
-        {
-            Debug.Log("Hit the Left Wall");
-            changeDirection = false;
-        }
-       
-            if (col.gameObject.tag == "Player")
+        if(col.gameObject.tag == "Player")
         {
             col.gameObject.GetComponent<Spaceship>().Damage();
             Die();
@@ -79,19 +65,4 @@ public class Enemy : MonoBehaviour
         temp.GetComponent<Bullet>().ChangeDirection();
         temp.GetComponent<Bullet>().ChangeColor(bulletcolor);
     }
-     public void moveEnemy()
-    {
-        
-        if(changeDirection == true)
-        {
-            rb.velocity = new Vector2(1, 0) * -1 * moveSpeed;
-        }
-        
-        else if (changeDirection == false)
-        {
-            rb.velocity = new Vector2(1, -1) * moveSpeed;
-        }
-        
-    }
-   
 }
